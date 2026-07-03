1b697d5ea7cd5b35f4f57381729a52739c751067
Deepanshu <deep.nalhera476@gmail.com>
2026-07-03 15:11:09 +0530

    sdfg

--- src/config.js
+++ src/config.js
@@ -1,7 +1,8 @@
-const API_URL =
-  process.env.REACT_APP_API_URL ||
-  (["localhost", "127.0.0.1"].includes(window.location.hostname)
-    ? "http://localhost:4000/"
-    : "https://drbskhealthcare.com/");
+// const API_URL = 'https://drbskhealthcare.com/';
 
-export default API_URL;
+const API_URL = 'http://localhost:4000/'
+
+// const API_URL = "https://j449fxt7-4000.inc1.devtunnels.ms/"
+
+
+export default API_URL; 
